de402683d3f9aa989f4649db07edfda169912c64
unknown <khalil.millwood@gmail.com>
2018-07-10 16:51:53 -0400

    add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
@@ -64,6 +65,33 @@ app.delete('/todos/:todoId', function(req, res) {
     }).catch((e)=> res.status(400).send());
 });
 
+app.patch('/todos/:id', function(req,res) {
+    var id = req.params.id;
+    //lodash - if properties exist pull them off req.body
+    //prevent unwanted properties from being updated
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findOneAndUpdate(id,{$set: body}, {new:true}).then(function(todo) {
+        if(!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch(function(e) {
+        res.status(400).send();
+    })
+});
 app.listen(port, function () {
     console.log(`Started on port ${port}`);
 });
